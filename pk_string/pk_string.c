7c69e88e0bc397c38de5e88ecd4f45b1edf0dcdf
Christopher Wong <christopherpk1996@gmail.com>
2017-06-03 23:47:35 +0800

    rebuild

--- pk_string/pk_string.c
+++ pk_string/pk_string.c
@@ -3,58 +3,48 @@
 struct String_t String = {
 	._constructor = &str_constructor,
 	._free = &str_free,
-	.append = &str_append,
 	.concat = &str_concat,
+	.c_str = &str_c_str,
 	.length = &str_length
 };
 
 string str_constructor(const char* str)
 {
-	size_t* header = (size_t*)(malloc(sizeof(size_t) + strlen(str) + 1));
-	char* data = (char*)(header + 1);
-	for (int i = 0; i != strlen(str); i++)
-	{
-		data[i] = str[i];
-	}
+	size_t* header = malloc(sizeof(size_t) + sizeof(char**));
+	char** data = (char**)(header + 1);
+	*data = strdup(str);
 	*header = strlen(str);
-	data[*header] = '\0';
 	return data;
 }
 
 void str_free(string this)
 {
 	size_t* header = (size_t*)(this) - 1;
+	free(*this);
 	free(header);
-}
-
-string str_append(string this, const string str)
-{
-	size_t* header = (size_t*)(realloc((size_t*)(this) - 1, sizeof(size_t) + String.length(this) + String.length(str) + 1));
-	char* data = (char*)(header + 1);
-	for (int i = 0; i != String.length(str); i++)
-	{
-		data[*header + i] = str[i];
-	}
-	*header  = *header + String.length(str);
-	data[*header] = '\0';
-	return data;
 }
 
 string str_concat(const string str1, const string str2)
 {
-	size_t* header = (size_t*)(malloc(sizeof(size_t) + String.length(str1) + String.length(str2) + 1));
-	char* data = (char*)(header + 1);
+	size_t* header = malloc(sizeof(size_t) + sizeof(char**));
+	char** data = (char**)(header + 1);
+	*data = malloc(String.length(str1) + String.length(str2) + 1);
 	for (int i = 0; i != String.length(str1); i++)
 	{
-		data[i] = str1[i];
+		(*data)[i] = String.c_str(str1)[i];
 	}
 	for (int i = 0; i != String.length(str2); i++)
 	{
-		data[String.length(str1) + i] = str2[i];
+		(*data)[String.length(str1) + i] = String.c_str(str2)[i];
 	}
 	*header = String.length(str1) + String.length(str2);
-	data[*header] = '\0';
+	(*data)[*header] = '\0';
 	return data;
+}
+
+const char* str_c_str(const string this)
+{
+	return *this;
 }
 
 size_t str_length(const string this)
--- pk_string/pk_string.h
+++ pk_string/pk_string.h
@@ -4,26 +4,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef char* string;
+typedef char** string;
 
 extern struct String_t String;
 
 struct String_t
 {
-	string (*_constructor)(const char* str);
+	string (*_constructor)(const char* const str);
 	void (*_free)(string this);
-	string (*append)(string this, const string str);
 	string (*concat)(const string str1, const string str2);
+	const char* (*c_str)(const string this);
 	size_t (*length)(const string this);
 };
 
 string str_constructor(const char* str);
 void str_free(string this);
 
-string str_append(string this, const string str);
-
 string str_concat(const string str1, const string str2);
 
+const char* str_c_str(const string this);
 size_t str_length(const string this);
 
 #endif
