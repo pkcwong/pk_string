0a0ebcc2b5a2ddf65b02360a4104176e84ef2286
Christopher Wong <christopherpk1996@gmail.com>
2017-06-04 19:37:31 +0800

    added substr, insert, append

--- pk_string/pk_string.c
+++ pk_string/pk_string.c
@@ -3,7 +3,12 @@
 struct String_t String = {
 	._constructor = &str_constructor,
 	._free = &str_free,
+	.append_c = &str_append_c,
+	.append = &str_append,
+	.insert_c = &str_insert_c,
+	.insert = &str_insert,
 	.concat = &str_concat,
+	.substr = &str_substr,
 	.c_str = &str_c_str,
 	.length = &str_length
 };
@@ -24,6 +29,58 @@ void str_free(string this)
 	free(header);
 }
 
+string str_append_c(string this, const char* str)
+{
+	size_t* header = (size_t*)(this) - 1;
+	*this = realloc(*this, String.length(this) + strlen(str) + 1);
+	strcpy(&((*this)[String.length(this)]), str);
+	*header = *header + strlen(str);
+	return this;
+}
+
+string str_append(string this, const string str)
+{
+	size_t* header = (size_t*)(this) - 1;
+	*this = realloc(*this, String.length(this) + String.length(str) + 1);
+	for (int i = 0; i != String.length(str); i++)
+	{
+		(*this)[String.length(this) + i] = String.c_str(str)[i];
+	}
+	*header = *header + String.length(str);
+	(*this)[*header] = '\0';
+	return this;
+}
+
+string str_insert_c(string this, size_t pos, const char* str)
+{
+	size_t* header = (size_t*)(this) - 1;
+	*this = realloc(*this, String.length(this) + strlen(str) + 1);
+	for (int i = String.length(this) + strlen(str); i != strlen(str) + pos - 1; i--)
+	{
+		(*this)[i] = (*this)[i - strlen(str)];
+	}
+	for (int i = 0; i != strlen(str); i++)
+	{
+		(*this)[i + pos] = str[i];
+	}
+	*header = *header + strlen(str);
+}
+
+string str_insert(string this, size_t pos, const string str)
+{
+	size_t* header = (size_t*)(this) - 1;
+	*this = realloc(*this, String.length(this) + String.length(str) + 1);
+	for (int i = String.length(this) + String.length(str); i != String.length(str) + pos - 1; i--)
+	{
+		(*this)[i] = (*this)[i - String.length(str)];
+	}
+	for (int i = 0; i != String.length(str); i++)
+	{
+		(*this)[i + pos] = String.c_str(str)[i];
+	}
+	*header = *header + String.length(str);
+}
+
 string str_concat(const string str1, const string str2)
 {
 	size_t* header = malloc(sizeof(size_t) + sizeof(char**));
@@ -38,6 +95,20 @@ string str_concat(const string str1, const string str2)
 		(*data)[String.length(str1) + i] = String.c_str(str2)[i];
 	}
 	*header = String.length(str1) + String.length(str2);
+	(*data)[*header] = '\0';
+	return data;
+}
+
+string str_substr(const string this, int iterator_start, int iterator_end)
+{
+	size_t* header = malloc(sizeof(size_t) + sizeof(char**));
+	char** data = (char**)(header + 1);
+	*data = malloc(iterator_end - iterator_start + 1);
+	for (int i = 0; i != iterator_end - iterator_start; i++)
+	{
+		(*data)[i] = String.c_str(this)[iterator_start + i];
+	}
+	*header = iterator_end - iterator_start;
 	(*data)[*header] = '\0';
 	return data;
 }
--- pk_string/pk_string.h
+++ pk_string/pk_string.h
@@ -10,9 +10,14 @@ extern struct String_t String;
 
 struct String_t
 {
-	string (*_constructor)(const char* const str);
+	string (*_constructor)(const char* str);
 	void (*_free)(string this);
+	string (*append_c)(string this, const char* str);
+	string (*append)(string this, const string str);
+	string (*insert_c)(string this, size_t pos, const char* str);
+	string (*insert)(string this, size_t pos, const string str);
 	string (*concat)(const string str1, const string str2);
+	string (*substr)(const string str, int iterator_start, int iterator_end);
 	const char* (*c_str)(const string this);
 	size_t (*length)(const string this);
 };
@@ -20,7 +25,13 @@ struct String_t
 string str_constructor(const char* str);
 void str_free(string this);
 
+string str_append_c(string this, const char* str);
+string str_append(string this, const string str);
+string str_insert_c(string this, size_t pos, const char* str);
+string str_insert(string this, size_t pos, const string str);
+
 string str_concat(const string str1, const string str2);
+string str_substr(const string this, int iterator_start, int iterator_end);
 
 const char* str_c_str(const string this);
 size_t str_length(const string this);
